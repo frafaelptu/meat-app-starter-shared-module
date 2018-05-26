c20f5bd0d1f563154efce7d6fe463e1320e61ef5
frafaelptu <frafaelptu@yahoo.com.br>
2018-05-26 19:06:26 -0300

    Feature module
    Feature module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -20,7 +20,6 @@ import { ShoppingCartServices } from './restaurant-detail/shopping-cart/shopping
 import { OrderComponent } from './order/order.component';
 import { OrderItemsComponent } from './order/order-items/order-items.component';
 import { OrderService } from './order/order.service';
-import { DeliveryCostsComponent } from './order/delivery-costs/delivery-costs.component';
 import { OrderSummaryComponent } from './order-summary/order-summary.component';
 import { SharedModule } from './shared/shared-module';
 
@@ -37,9 +36,6 @@ import { SharedModule } from './shared/shared-module';
     ShoppingCartComponent,
     MenuItemComponent,
     ReviewsComponent,
-    OrderComponent,
-    OrderItemsComponent,
-    DeliveryCostsComponent,
     OrderSummaryComponent
   ],
   imports: [
