1f33d4339f886dfcd25b1063dce0421fb46460da
frafaelptu <frafaelptu@yahoo.com.br>
2018-05-26 20:26:50 -0300

    Snackbar
    Criação da snackbar sem animação

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { NgModule, LOCALE_ID } from '@angular/core';
 import { HttpModule } from '@angular/http';
 import { RouterModule, PreloadAllModules } from '@angular/router';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import { ROUTES } from './app.routes';
 
 import { AppComponent } from './app.component';
@@ -17,6 +18,8 @@ import { MenuItemComponent } from './restaurant-detail/menu-item/menu-item.compo
  import { ReviewsComponent } from './restaurant-detail/reviews/reviews.component';
 import { OrderSummaryComponent } from './order-summary/order-summary.component';
 import { SharedModule } from './shared/shared-module';
+
+
 
 
 @NgModule({
@@ -35,6 +38,7 @@ import { SharedModule } from './shared/shared-module';
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     HttpModule,
     FormsModule,
     ReactiveFormsModule,
--- src/app/shared/shared-module.ts
+++ src/app/shared/shared-module.ts
@@ -7,13 +7,14 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { ShoppingCartServices } from '../restaurant-detail/shopping-cart/shopping-cart.services';
 import { RestaurantsServices } from 'app/restaurants/restaurants.services';
 import { OrderService } from '../order/order.service';
+import { SnackbarComponent } from './messages/snackbar/snackbar.component';
 
 
 @NgModule({
-    declarations: [InputComponent, RadioComponent, RatingComponent],
+    declarations: [InputComponent, RadioComponent, RatingComponent, SnackbarComponent],
     imports: [CommonModule, FormsModule, ReactiveFormsModule],
     exports: [InputComponent, RadioComponent, RatingComponent,
-            CommonModule, FormsModule, ReactiveFormsModule]
+            SnackbarComponent, CommonModule, FormsModule, ReactiveFormsModule]
 })
 
 export class SharedModule{
