02de330a42008898db64a68d8ec26557916ccc4c
frafaelptu <frafaelptu@yahoo.com.br>
2018-05-26 21:21:07 -0300

    snackbar com operador Do e SwicthMap
    snackbar com operador Do e SwicthMap para resolver o problema de vários subscribe com o timer

--- src/app/shared/messages/snackbar/snackbar.component.ts
+++ src/app/shared/messages/snackbar/snackbar.component.ts
@@ -1,43 +1,58 @@
-import { Component, OnInit } from '@angular/core';
-import { trigger, state, style, transition, animate } from '@angular/animations';
-import { NotificantionServices } from '../notification.services';
-import { Observable } from 'rxjs/Observable';
-import 'rxjs/add/observable/timer'
+import { Component, OnInit } from "@angular/core";
+import {
+  trigger,
+  state,
+  style,
+  transition,
+  animate
+} from "@angular/animations";
+import { NotificantionServices } from "../notification.services";
+import { Observable } from "rxjs/Observable";
+import "rxjs/add/observable/timer";
+import "rxjs/add/operator/do";
+import "rxjs/add/operator/switchMap";
 
 @Component({
-  selector: 'mt-snackbar',
-  templateUrl: './snackbar.component.html',
-  styleUrls: ['./snackbar.component.css'],
+  selector: "mt-snackbar",
+  templateUrl: "./snackbar.component.html",
+  styleUrls: ["./snackbar.component.css"],
   animations: [
-    trigger('snack-visibility', [
-      state('hidden', style({
-        opacity: 0,
-        bottom: '0px'
-      })),
-      state('vibible', style({
-        opacity: 1,
-        bottom: '30px'
-      })),
-      transition('hidden => visible', animate('500ms 0s ease-in')),
-      transition('visible => hidden', animate('500ms 0s ease-out'))
+    trigger("snack-visibility", [
+      state(
+        "hidden",
+        style({
+          opacity: 0,
+          bottom: "0px"
+        })
+      ),
+      state(
+        "visible",
+        style({
+          opacity: 1,
+          bottom: "30px"
+        })
+      ),
+      transition("hidden => visible", animate("500ms 0s ease-in")),
+      transition("visible => hidden", animate("500ms 0s ease-out"))
     ])
   ]
 })
 export class SnackbarComponent implements OnInit {
+  message: string = "Hello there!";
+  snackVisibility: string = "hidden";
 
-  message: string = 'Hello there!'
-  snackVisibility: string = 'hidden'
-
-  constructor(private notificationService: NotificantionServices) { }
+  constructor(private notificationService: NotificantionServices) {}
 
   ngOnInit() {
-    this.notificationService.notifier.subscribe(message =>  {
-      this.message = message
-      this.snackVisibility = 'visible'
-      Observable.timer(3000).subscribe(timer => {
-        this.snackVisibility = 'hidden'
+    //operador "do" permite executar uma ação no momento que chega a mensagem
+    this.notificationService.notifier
+      .do(message=>{
+        this.message = message;
+        this.snackVisibility = "visible";
+        //operador "swicthMap" troca o Observable inteiro sendo assim, não fica vários timer concorrendo
+        //Quando chega uma nova mensagem o switchMap identifica que existe um Observable antigo ele faz o un-subscribe do Observable
       })
-    })
+      .switchMap(message => Observable.timer(3000))
+      .subscribe(timer => (this.snackVisibility = "hidden"));
   }
-
 }
